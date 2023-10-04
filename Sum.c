65d254d60398965477a7b0a013311e23264bbd4c
Ajhar Anjum Ahmed <139153584+ajharanjumahmed@users.noreply.github.com>
2023-10-04 22:49:32 +0600

    Sum

---
+++ Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c,t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        if(a+b==c || b+c==a || c+a==b){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+
+}
